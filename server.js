dc539a4a414305172da0937ebf3c852a5d1a2dbd
Ayazuddin <46484289+ayazuddin007@users.noreply.github.com>
2020-08-31 01:30:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">Java Home App - version-10!!</h1> <h1 style="color:red;">Node Js App Using Pipeline</h1>\n');
+  res.send('<h1 style="color:green;text-align:center">Welcome to Click 2 Cloud !!! </h1> <h2 style="color:red;text-align:center">Node Js Application Using Pipeline</h2>\n');
 });
 
 
