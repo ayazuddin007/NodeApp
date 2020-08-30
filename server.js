3ad1016e449dc4c419dca2dcdf830b6efebc5bdb
Ayazuddin <46484289+ayazuddin007@users.noreply.github.com>
2020-08-31 02:17:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;text-align:center">Welcome to Click 2 Cloudddd !!! </h1> <h2 style="color:red;text-align:center">Node Js Application Using DevOps CI CD Pipeline</h2> <h2 style="color:black;text-align:center">From: Ayazuddin Ejazuddin</h2>\n');
+  res.send('<h1 style="color:green;text-align:center">Welcome to Click 2 Cloud !!! </h1> <h2 style="color:red;text-align:center">Node Js Application Using DevOps CI CD Pipeline</h2> <h2 style="color:black;text-align:center">From: Ayazuddin Ejazuddin</h2>\n');
 });
 
 
